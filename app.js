3d6e89efcc88bc6c4431a82214b5179cd97621ce
orkhan.verdiev <orkhan.verdiev@cibirlan.com>
2023-04-26 20:45:36 +0200

    fix

--- app.js
+++ app.js
@@ -6,9 +6,15 @@ const port = process.env.PORT || 3000;
 
 // Использование middleware cors
 app.use(cors());
+let test = "x";
 
 app.get("/", (req, res) => {
-  res.send("Arduino arduinooo 3");
+  res.send(JSON.stringify(test));
+});
+
+app.post("/users", (req, res) => {
+  test = req.body;
+  res.status(201).json({ message: "User created successfully", data: test });
 });
 
 app.listen(port, () => {
