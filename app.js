c0bc9c58d8b40bc639db395e369245bad172e535
Aleksandr Kolomytsev <tzhz@mail.ru>
2023-04-27 20:47:00 +0200

    add db

--- app.js
+++ app.js
@@ -1,4 +1,6 @@
 const express = require("express");
+const fs = require("fs");
+const bodyParser = require("body-parser");
 const cors = require("cors"); // импорт cors middleware
 const app = express();
 
@@ -6,15 +8,30 @@ const port = process.env.PORT || 3000;
 
 // Использование middleware cors
 app.use(cors());
+app.use(bodyParser.json());
 let test = "x";
-
-app.get("/", (req, res) => {
-  res.send(JSON.stringify(test));
-});
+const dataDB = fs.readFileSync('./db.json', 'utf-8');
+// Преобразование содержимого в объект JavaScript
+const jsonDataGet = JSON.parse(dataDB);
 
 app.post("/users", (req, res) => {
   test = req.body;
   res.status(201).json({ message: "User created successfully", data: test });
+
+  let curJsonFileDate = JSON.parse(fs.readFileSync('./db.json', 'utf-8'));
+  curJsonFileDate.push(test)
+  curJsonFileDate = JSON.stringify(curJsonFileDate);
+
+  fs.writeFile('./db.json', curJsonFileDate, (err) => {
+    if (err) throw err;
+    console.log('Data saved to file');
+  });
+});
+
+
+app.get("/", (req, res) => {
+  const getCurJsonFileDate = JSON.parse(fs.readFileSync('./db.json', 'utf-8'));
+  res.send(JSON.stringify(getCurJsonFileDate));
 });
 
 app.listen(port, () => {
